dbb06e3cb6def7f06b1b6bc3a782a50f9aeb054a
venkatsgithub1 <venkatsgithub@gmail.com>
2017-12-24 17:22:40 +0530

    Update sorted-union.js

--- JS/IntermediateAlgoScripting/sorted-union.js
+++ JS/IntermediateAlgoScripting/sorted-union.js
@@ -2,7 +2,7 @@ function uniteUnique(arr) {
   	let final_arr=[];
 	for(let i=0;i<arguments.length;i++) {
 		for(let j=0;j<arguments[i].length;j++) {
-			let data=arguments[i][j]
+			let data=arguments[i][j];
 			if(final_arr.includes(data)) {
 				continue;
 			}
